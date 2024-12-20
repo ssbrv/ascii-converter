001433816fc1f50b3128c843c643b33470d0c3ae
Daniyar Sabirov <sabirdan@cvut.cz>
2024-12-20 17:10:09 +0100

    Fix ascii convertion

--- src/main/scala/converter/service/ascii/linear/LinearAsciiConverter.scala
+++ src/main/scala/converter/service/ascii/linear/LinearAsciiConverter.scala
@@ -7,10 +7,11 @@ import converter.service.ascii.AsciiConverter
 class LinearAsciiConverter(val gradient: String) extends MediaConverterImpl[GreyscaleValue, Char] with AsciiConverter {
     
   require(gradient != null && gradient.nonEmpty, "Provided empty gradient for Ascii Linear Converter.")
-  require(gradient.length <= 255, "Provided too long gradient for Ascii Linear Converter. It has to be 255 characters at most.")
-  
-  override protected def convert(greyscale: GreyscaleValue): Char = {
-    val index = Math.round((greyscale.value / 255.0) * (gradient.length - 1)).toInt
-    gradient.charAt(index)
+  require(gradient.length <= 256, s"Provided too long gradient of length ${gradient.length} for Ascii Linear Converter. It has to be 256 characters at most.")
+  private val step = 256.0 / gradient.length
+
+  override def convert(greyscale: GreyscaleValue): Char = {
+    val index = greyscale.value / step
+    gradient.charAt(index.toInt)
   }
 }
--- src/test/scala/converter/service/ascii/linear/LinearAsciiConverterTest.scala
+++ src/test/scala/converter/service/ascii/linear/LinearAsciiConverterTest.scala
@@ -1,7 +1,47 @@
 package converter.service.ascii.linear
 
+import common.domain.greyscale.GreyscaleValue
 import org.scalatest.funsuite.AnyFunSuite
 
 class LinearAsciiConverterTest extends AnyFunSuite {
-  
+
+  test("Throws an exception for null gradient") {
+    assertThrows[IllegalArgumentException] {
+      new LinearAsciiConverter(null)
+    }
+  }
+
+  test("Throws an exception for empty gradient") {
+    assertThrows[IllegalArgumentException] {
+      new LinearAsciiConverter("")
+    }
+  }
+
+  test("Throws an exception for gradient longer than 256 characters") {
+    val longGradient = "a" * 257
+    assertThrows[IllegalArgumentException] {
+      new LinearAsciiConverter(longGradient)
+    }
+  }
+
+  def testGradient(gradient: String): Unit = {
+    test(s"Test all greyscale values for gradient: $gradient") {
+      val step = 256.0 / gradient.length
+
+      val expectedResult = Array.tabulate(256) { greyscaleValue =>
+        val index = Math.min((greyscaleValue / step).toInt, gradient.length - 1)
+        gradient.charAt(index)
+      }
+
+      val converter = new LinearAsciiConverter(gradient)
+
+      for (greyscaleValue <- 0 to 255)
+        assert(converter.convert(GreyscaleValue(greyscaleValue)) == expectedResult(greyscaleValue))
+    }
+  }
+
+  testGradient("abcd")
+  testGradient("123456")
+  testGradient("1".repeat(100))
+  testGradient("1234".repeat(64))
 }
